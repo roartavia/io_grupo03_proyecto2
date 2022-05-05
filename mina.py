461e857b40f441b6cc5c89dbb774e473aa60e225
Rodolfo Artavia <rodolfoartavia@Rodolfos-MacBook-Pro.local>
2022-05-05 10:43:02 -0600

    fix: fixed mina brute force use step(1) to go from one mina to another

--- mina.py
+++ mina.py
@@ -57,6 +57,15 @@ def printAnswer(value, routesStr):
     print (f'\nOutput : {value}')
     print (f'Ejemplo de selección de casillas: \n{routesStr}\n')
 
+def getTransposeMatrix(matrix):
+    numCols = len(matrix[0]) 
+    cols = [[] for i in range(numCols)]
+    for idxRow, row in enumerate(matrix):
+        for i in range(numCols):
+            cols[i].append((row[i], idxRow))
+    return cols
+
+
 def runBruteForce(lines):
     print('Run brute force')
     # Basically is a multiplications of arrays, each needed correspond to 
@@ -65,30 +74,36 @@ def runBruteForce(lines):
 
     # The way you need to give the answer you need a tuple yo give the row you choose, 
     # use an obj same as the contenedor(?) (value, row)
-    numCols = len(lines[0]) 
-    cols = [[] for i in range(numCols)]
-    for idxRow, row in enumerate(lines):
-        for i in range(numCols):
-            cols[i].append((row[i], idxRow))
-
+    cols = getTransposeMatrix(lines)
     permutations = list(itertools.product(*cols))
     # have a list of indexes that have the indexes of the permutations that have the maxValue
     # have the maxValue stored
     maxGold = 0
     maxIndexes = []
     for i, per in enumerate(permutations):
+        isValid = True
         stepsTotal = 0
-        for step in per: 
-            stepsTotal+=step[0]
+        # You need to check first if you can go from this index to the next step
+        for j,step in enumerate(per):
+            # if index is -1 or +1 of prev then is a valid move 
+            if j != 0:
+                if step[1] - 1 == per[j - 1][1] or step[1] + 1 == per[j - 1][1] or step[1] == per[j - 1][1]:
+                    stepsTotal += step[0]
+                else:
+                    isValid = False
+                    break
+            else:
+                stepsTotal += step[0]
         # if sum of this per is greater clean the list and chagne the maxGold
         # if the sum is equal then add this to max indexes
         # else ignore
-        if stepsTotal > maxGold:
-            maxGold = stepsTotal
-            maxIndexes = []
-            maxIndexes.append(i)
-        elif stepsTotal == maxGold:
-            maxIndexes.append(i)
+        if isValid:
+            if stepsTotal > maxGold:
+                maxGold = stepsTotal
+                maxIndexes = []
+                maxIndexes.append(i)
+            elif stepsTotal == maxGold:
+                maxIndexes.append(i)
     finalRoutes = str(permutations[maxIndexes[0]]).replace("),", ") ->")
     for i in range(1,len(maxIndexes)):
         finalRoutes += f' OR \n{str(permutations[maxIndexes[i]]).replace("),", ") ->")}'
@@ -98,6 +113,9 @@ def runBruteForce(lines):
 
 def runDynamic(lines):
     print('Run dynamic programming algoritm')
+    # Do the cols for rows, then for each new row get the max possible add this max to the matrix of ans
+    # do the same for the n new rows, then you can the route
+
 
 # Run the project
 main() 
