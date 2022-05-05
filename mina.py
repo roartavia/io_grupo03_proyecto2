8e1866b88635afff4c60d136b97f6fdf05d87805
Rodolfo Artavia <rodolfoartavia@Rodolfos-MacBook-Pro.local>
2022-05-05 08:20:43 -0600

    feat: added brute force for mina algoritm

--- mina.py
+++ mina.py
@@ -49,18 +49,52 @@ def main():
     print(
         f'{BASH_COLORS.FAIL}Tiempo de ejecución: {"{:.8f}".format(time.time() - startTime)} segundos.{BASH_COLORS.ENDC}\n')
 
-def printAnswer(h, bagStr):
+def printAnswer(value, routesStr):
     # Output : 16
     # Ejemplo de selección de casillas:
     # (2,0) -> (1,1) -> (1,2) -> (0,3) OR
     # (2,0) -> (3,1) -> (2,2) -> (2,3)
-    # Basically is a multiplications of arrays, each needed correspond to 
-    # each column, that gives all the possible permutations
-    # then sum each array result and get the ones with the max possible sum
-    print (f'\nThis\n')
+    print (f'\nOutput : {value}')
+    print (f'Ejemplo de selección de casillas: \n{routesStr}\n')
 
 def runBruteForce(lines):
     print('Run brute force')
+    # Basically is a multiplications of arrays, each needed correspond to 
+    # each column, that gives all the possible permutations
+    # then sum each array result and get the ones with the max possible sum
+
+    # The way you need to give the answer you need a tuple yo give the row you choose, 
+    # use an obj same as the contenedor(?) (value, row)
+    numCols = len(lines[0]) 
+    cols = [[] for i in range(numCols)]
+    for idxRow, row in enumerate(lines):
+        for i in range(numCols):
+            cols[i].append((row[i], idxRow))
+
+    permutations = list(itertools.product(*cols))
+    # have a list of indexes that have the indexes of the permutations that have the maxValue
+    # have the maxValue stored
+    maxGold = 0
+    maxIndexes = []
+    for i, per in enumerate(permutations):
+        stepsTotal = 0
+        for step in per: 
+            stepsTotal+=step[0]
+        # if sum of this per is greater clean the list and chagne the maxGold
+        # if the sum is equal then add this to max indexes
+        # else ignore
+        if stepsTotal > maxGold:
+            maxGold = stepsTotal
+            maxIndexes = []
+            maxIndexes.append(i)
+        elif stepsTotal == maxGold:
+            maxIndexes.append(i)
+    finalRoutes = str(permutations[maxIndexes[0]]).replace("),", ") ->")
+    for i in range(1,len(maxIndexes)):
+        finalRoutes += f' OR \n{str(permutations[maxIndexes[i]]).replace("),", ") ->")}'
+
+    printAnswer(maxGold, finalRoutes)
+
 
 def runDynamic(lines):
     print('Run dynamic programming algoritm')
