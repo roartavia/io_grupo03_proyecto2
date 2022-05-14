8e6e57c982400e476cba740ba78772a24ffd0606
Bryan Miranda <miranda@estudiantec.cr>
2022-05-14 14:28:25 -0700

    container dynamic

--- contenedor.py
+++ contenedor.py
@@ -4,6 +4,7 @@ import os.path
 import time
 from helper import BASH_COLORS, printHelp
 
+
 class Articule:
     def __init__(self, key, weight, value):
         self.key = key
@@ -13,8 +14,8 @@ class Articule:
     def show(self):
         return f'{self.key + 1}, '
 
-def main():
 
+def main():
     if len(sys.argv) <= 1:
         printHelp('contenedor')
         return
@@ -40,17 +41,19 @@ def main():
         matrix.append(row)
 
     startTime = time.time()
-    if mode == '1':        
+    if mode == '1':
         runBruteForce(matrix)
     else:
         runDynamic(matrix)
     print(
         f'{BASH_COLORS.FAIL}Tiempo de ejecución: {"{:.8f}".format(time.time() - startTime)} segundos.{BASH_COLORS.ENDC}\n')
 
+
 def printAnswer(h, bagStr):
-    print (f'\nBeneficio máximo: {BASH_COLORS.OKGREEN}{h}{BASH_COLORS.ENDC}.')
-    print (f'Incluidos: {BASH_COLORS.OKGREEN}{bagStr}{BASH_COLORS.ENDC}\n')
-    
+    print(f'\nBeneficio máximo: {BASH_COLORS.OKGREEN}{h}{BASH_COLORS.ENDC}.')
+    print(f'Incluidos: {BASH_COLORS.OKGREEN}{bagStr}{BASH_COLORS.ENDC}\n')
+
+
 def runBruteForce(lines):
     print('Run brute force')
 
@@ -59,7 +62,7 @@ def runBruteForce(lines):
     for key, item in enumerate(lines[1:]):
         articules.append(Articule(key, weight=item[0], value=item[1]))
 
-    permutations = (list(itertools.permutations(articules)))    
+    permutations = (list(itertools.permutations(articules)))
     maxValues = []
 
     for per in permutations:
@@ -88,9 +91,45 @@ def runBruteForce(lines):
             maxH = newHeight
         else:
             break
-    printAnswer(h=maxValue,bagStr=filledBag)
+    printAnswer(h=maxValue, bagStr=filledBag)
+
+
+def runDynamic(lines):
+    print('Run dynamic programming algorithm')
+
+    bagWeight = lines[0][0]
+    articules = []
+
+    for key, item in enumerate(lines[1:]):
+        articules.append(Articule(key, weight=item[0], value=item[1]))
+
+    dpMatrix = [[[0, []] for i in range(bagWeight + 1)] for j in range(len(articules) + 1)]
+
+    for i in range(len(articules) + 1):
+        for w in range(bagWeight + 1):
+            if i != 0 and w != 0:
+                if articules[i - 1].weight <= w != 0 and i != 0:
+                    (dpMatrix[i][w])[0] = max(
+                        articules[i - 1].value + (dpMatrix[i - 1][w - articules[i - 1].weight])[0],
+                        (dpMatrix[i - 1][w])[0])
+                    currentValue = articules[i - 1].value + (dpMatrix[i - 1][w - articules[i - 1].weight])[0]
+                    if currentValue > (dpMatrix[i - 1][w])[0]:
+                        (dpMatrix[i][w])[1] = (dpMatrix[i - 1][w - articules[i - 1].weight])[1] + [articules[i - 1]]
+                        (dpMatrix[i][w])[0] = currentValue
+                    else:
+                        (dpMatrix[i][w])[1] = (dpMatrix[i - 1][w])[1]
+                        (dpMatrix[i][w])[0] = currentValue
+                else:
+                    (dpMatrix[i][w])[0] = (dpMatrix[i - 1][w])[0]
+                    (dpMatrix[i][w])[1] = (dpMatrix[i - 1][w])[1]
+    maxValue = dpMatrix[len(articules)][bagWeight][0]
+    col = dpMatrix[len(articules)][bagWeight][1]
+    ans = col[0].show()
+    for i in range(1, len(col)):
+        a = col[i]
+        ans = f'{ans}{a.show()}'
+    print(Answer(h=maxValue, bagStr=ans))
+
 
 # Run the project
-main() 
-
- 
+main()
